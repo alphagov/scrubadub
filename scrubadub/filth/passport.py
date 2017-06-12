09cd4373b3b0cfaf9d26f503fe91a9a69b6b534a
Matthew Upson <matthew.upson@digital.cabinet-office.gov.uk>
2017-06-12 23:05:55 +0100

    Deal with filth clashes in tests
    The passport filth recognises a long integer as being a passport number.
    Clearly this will clash with a number of other filths, for example phone
    numbers and credit card numbers.
    
    Simplify the tests to allow the outcome of each test to specified
    individually, and change expection to deal with clashes.

--- scrubadub/filth/passport.py
+++ scrubadub/filth/passport.py
@@ -10,5 +10,7 @@ class PassportFilth(RegexFilth):
     # http://regexlib.com/REDetails.aspx?regexp_id=2390
 
     regex = re.compile((
-        "[0-9]{10}(?:((GBR)|(GBD)|(GBO)|(GBS)|(GBP)|(GBN))[0-9]{7}[U,M,F]{1}[0-9]{7,9})?"
+        "[0-9]{10}"                                 # First 10 digits
+        "(?:((GBR)|(GBD)|(GBO)|(GBS)|(GBP)|(GBN))"  # Country issue code
+        "[0-9]{7}[U,M,F]{1}[0-9]{7,9})?"            # Suffix
     ), re.VERBOSE)
